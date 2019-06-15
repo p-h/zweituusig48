b7c8010cd70cfe8f83f03f41167114d9eb9d6166
Philippe Hürlimann <p@hurlimann.org>
2019-06-15 22:02:33 +0200

    switched to list based grids

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,66 +1,47 @@
 {-# LANGUAGE OverloadedStrings, OverloadedLabels, MultiWayIf #-}
 
-module Lib ( someFunc
-           , updateGrid
-           , Direction (..)
-           , rotateGridLeft
-           , rotateGridRight
-           , Row (..)
-           , Grid (..)
-           , reverse'
-           , reverse''
-           , reverseColumns
-           , grid
-           ) where
+module Lib
+    ( someFunc
+    , updateGrid
+    , Direction(..)
+    , Grid
+    , Row
+    , rotateGridLeft
+    , rotateGridRight
+    , reverseColumns
+    ) where
 
-import Control.Monad(forever)
+import Control.Monad (forever)
 import Data.GI.Base
-import Data.List(genericIndex)
-import Data.Tuple.Select
-import Data.Word(Word32)
+import Data.List (genericIndex)
 import qualified Data.Text as T
+import Data.Tuple.Select
+import Data.Word (Word32)
 import qualified GI.Gdk as Gdk
 import qualified GI.Gtk as Gtk
 import qualified Reactive.Banana as B
 import qualified Reactive.Banana.Frameworks as B
 import qualified System.Random as R
 
-data Direction = North | East | South | West
+data Direction
+    = North
+    | East
+    | South
+    | West
     deriving (Eq, Show, Read)
 
-reverse' :: Grid a -> Grid a
-reverse' (Grid (e1, e2, e3, e4)) = Grid (e4, e3, e2, e1)
-
-reverse'' :: Row a -> Row a
-reverse'' (Row (e1, e2, e3, e4)) = Row (e4, e3, e2, e1)
-
 reverseColumns :: Grid a -> Grid a
-reverseColumns (Grid (r1, r2, r3, r4)) = Grid (reverse'' r1, reverse'' r2, reverse'' r3, reverse'' r4)
+reverseColumns = map reverse
 
-newtype Row a = Row (a, a, a, a)
-    deriving (Show, Eq)
+type Row a = [a]
 
-newtype Grid a = Grid (Row a, Row a, Row a, Row a)
-    deriving (Show, Eq)
-
-grid :: ((a, a, a, a), (a, a, a, a), (a, a, a, a), (a, a, a, a)) -> Grid a
-grid (a, b, c, d) = Grid (Row a, Row b, Row c, Row d)
+type Grid a = [Row a]
 
 initialGrid :: Grid Int
-initialGrid = Grid (Row (1, 2, 3, 4), Row (1, 2, 3, 4), Row (1, 2, 3, 4), Row (1, 2, 3, 4))
-
---modifyGrid :: Integral i => a -> i -> i -> Grid a
---modifyGrid a x y = let row = sey g in upx a
-    --where
-        --upx = updators `genericIndex` x
-        --upy = updators `genericIndex` y
-        --sey = selectors `genericIndex` y
-
-getters :: [(a, a, a, a) -> a]
-getters = [sel1, sel2, sel3, sel4]
+initialGrid = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
 
 gridLookup :: Integral i => i -> i -> Grid a -> a
-gridLookup x y (Grid t) = let Row r = (getters `genericIndex` y) t in (getters `genericIndex` x) r
+gridLookup x y g = g `genericIndex` y `genericIndex` x
 
 bindings =
     [ (Gdk.KEY_Up, North)
@@ -81,35 +62,38 @@ keyCodeToDirection :: Word32 -> Maybe Direction
 keyCodeToDirection keycode = lookup keycode bindings
 
 someFunc :: IO ()
-someFunc = do 
+someFunc = do
     Gtk.init Nothing
-    window <- new Gtk.ApplicationWindow [ #title := "Reactive banana and gtk test"
-                                        , #windowPosition := Gtk.WindowPositionCenter ]
+    window <-
+        new
+            Gtk.ApplicationWindow
+            [ #title := "Reactive banana and gtk test"
+            , #windowPosition := Gtk.WindowPositionCenter
+            ]
     on window #destroy Gtk.mainQuit
-
-    labelGrid <- new Gtk.Grid [ #expand := True
-                              , #halign := Gtk.AlignFill
-                              , #valign := Gtk.AlignFill ]
+    labelGrid <-
+        new
+            Gtk.Grid
+            [ #expand := True
+            , #halign := Gtk.AlignFill
+            , #valign := Gtk.AlignFill
+            ]
     #add window labelGrid
-
-    labels <- sequence
-        [
-            let coord = (x, y)
-                t = T.pack $ show coord
-            in (,) coord <$> new Gtk.Label [ #label := t, #expand := True ]
-        | x <- [0..3], y <- [0..3]
-        ]
-
+    labels <-
+        sequence
+            [ let coord = (x, y)
+                  t = T.pack $ show coord
+               in (,) coord <$> new Gtk.Label [#label := t, #expand := True]
+            | x <- [0 .. 3]
+            , y <- [0 .. 3]
+            ]
     mapM_ (\((x, y), label) -> #attach labelGrid label x y 1 1) labels
-
     (keyEvent, fireKey) <- B.newAddHandler
     on window #keyPressEvent $ \eventKey -> do
         kv <- get eventKey #keyval
         fireKey kv
         pure True
-
     #showAll window
-
     let networkDescription :: B.MomentIO ()
         networkDescription = do
             eKey <- B.fromAddHandler keyEvent
@@ -117,76 +101,60 @@ someFunc = do
             let eGridChange = fmap updateGrid eDir
             eGrid <- B.accumE initialGrid eGridChange
             B.reactimate $ (`displayGrid` labels) <$> eGrid
-
     network <- B.compile networkDescription
-
     B.actuate network
-
     Gtk.main
 
-
-displayGrid :: (Integral i, Num a, Show a) => Grid a -> [((i, i), Gtk.Label)] -> IO ()
-displayGrid grid = mapM_ (\((x, y), label) ->
-    let val = T.pack $ show $ gridLookup x y grid
-    in set label [#label := val])
+displayGrid ::
+       (Integral i, Num a, Show a) => Grid a -> [((i, i), Gtk.Label)] -> IO ()
+displayGrid grid =
+    mapM_
+        (\((x, y), label) ->
+             let val = T.pack $ show $ gridLookup x y grid
+              in set label [#label := val])
 
 updateGrid :: (Eq a, Num a) => Direction -> Grid a -> Grid a
-updateGrid dir grid@(Grid (r1, r2, r3, r4)) = case dir of
-    East -> Grid (updateEast r1, updateEast r2, updateEast r3, updateEast r4)
-    South -> rotateGridRight $ updateGrid East $ rotateGridLeft grid
-    West -> reverseColumns $ updateGrid East $ reverseColumns grid
-    North -> reverse' $ updateGrid South $ reverse' grid
-    where
-        updateEast :: (Eq a, Num a) => Row a -> Row a
-        updateEast (Row (a, b, c, d)) = Row
-            if | a /= 0 && c /= 0 && a == b && c == d -> (0, 0, a + 1, c + 1)
-               | c /= 0 && c == d -> (0, a, b, c + 1)
-               | b /= 0 && b == c -> (0, a, b + 1, d)
-               | a /= 0 && a == b -> (0, a + 1, c, d)
-               | a == 0 && b == 0 && d == 0 -> (0, 0, 0, c)
-               | a == 0 && c == 0 && d == 0 -> (0, 0, 0, b)
-               | b == 0 && c == 0 && d == 0 -> (0, 0, 0, a)
-               | a == 0 && b == 0 -> (0, 0, c, d)
-               | a == 0 && c == 0 -> (0, 0, b, d)
-               | a == 0 && d == 0 -> (0, 0, b, c)
-               | b == 0 && c == 0 -> (0, 0, a, d)
-               | b == 0 && d == 0 -> (0, 0, a, c)
-               | c == 0 && d == 0 -> (0, 0, a, b)
-               | b == 0 -> (0, a, c, d)
-               | c == 0 -> (0, a, b, d)
-               | d == 0 -> (0, a, b, c)
-               | otherwise -> (a, b, c, d)
-
---extendGrid :: Num a => Grid a -> IO (Grid a)
---extendGrid grid = do
+updateGrid dir grid =
+    case dir of
+        East -> map updateEast grid
+        South -> rotateGridRight $ updateGrid East $ rotateGridLeft grid
+        West -> reverseColumns $ updateGrid East $ reverseColumns grid
+        North -> reverse $ updateGrid South $ reverse grid
+  where
+    updateEast :: (Eq a, Num a) => Row a -> Row a
+    updateEast (a:b:c:d:_) =
+        if | a /= 0 && c /= 0 && a == b && c == d -> [0, 0, a + 1, c + 1]
+           | c /= 0 && c == d -> [0, a, b, c + 1]
+           | b /= 0 && b == c -> [0, a, b + 1, d]
+           | a /= 0 && a == b -> [0, a + 1, c, d]
+           | a == 0 && b == 0 && d == 0 -> [0, 0, 0, c]
+           | a == 0 && c == 0 && d == 0 -> [0, 0, 0, b]
+           | b == 0 && c == 0 && d == 0 -> [0, 0, 0, a]
+           | a == 0 && b == 0 -> [0, 0, c, d]
+           | a == 0 && c == 0 -> [0, 0, b, d]
+           | a == 0 && d == 0 -> [0, 0, b, c]
+           | b == 0 && c == 0 -> [0, 0, a, d]
+           | b == 0 && d == 0 -> [0, 0, a, c]
+           | c == 0 && d == 0 -> [0, 0, a, b]
+           | b == 0 -> [0, a, c, d]
+           | c == 0 -> [0, a, b, d]
+           | d == 0 -> [0, a, b, c]
+           | otherwise -> [a, b, c, d]
     --n <- (\r -> if r == 0 then 2 else 1) <$> R.randomRIO(0, 9)
-
     --let choices = [(x, y) | x <- [0..3], y <- [0..3], gridLookup x y == 0]
-
     --i <- R.randomRIO (0, length choices - 1)
-
     --let (x, y) = choices !! i
-
     --let newGrid = (modifyGrid x y) n grid
     --pure newGrid
 
-
+--extendGrid :: Num a => Grid a -> IO (Grid a)
+--extendGrid grid = do
 rotateGridRight :: Grid a -> Grid a
-rotateGridRight (Grid g) = 
-    let (Row (a1, a2, a3, a4), Row (b1, b2, b3, b4), Row (c1, c2, c3, c4), Row (d1, d2, d3, d4)) = g
-    in grid
-        ((d1, c1, b1, a1)
-        ,(d2, c2, b2, a2)
-        ,(d3, c3, b3, a3)
-        ,(d4, c4, b4, a4)
-        )
+rotateGridRight g =
+    let ((a1:a2:a3:a4:_):(b1:b2:b3:b4:_):(c1:c2:c3:c4:_):(d1:d2:d3:d4:_):_) = g
+     in [[d1, c1, b1, a1], [d2, c2, b2, a2], [d3, c3, b3, a3], [d4, c4, b4, a4]]
 
 rotateGridLeft :: Grid a -> Grid a
-rotateGridLeft (Grid g) =
-    let (Row (a1, a2, a3, a4), Row (b1, b2, b3, b4), Row (c1, c2, c3, c4), Row (d1, d2, d3, d4)) = g
-    in grid
-        ((a4, b4, c4, d4)
-        ,(a3, b3, c3, d3)
-        ,(a2, b2, c2, d2)
-        ,(a1, b1, c1, d1)
-        )
+rotateGridLeft g =
+    let ((a1:a2:a3:a4:_):(b1:b2:b3:b4:_):(c1:c2:c3:c4:_):(d1:d2:d3:d4:_):_) = g
+     in [[a4, b4, c4, d4], [a3, b3, c3, d3], [a2, b2, c2, d2], [a1, b1, c1, d1]]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,253 +1,105 @@
+import Control.Monad (forM_)
 import Lib
 import Test.Hspec
 import Test.QuickCheck
-import Control.Monad(forM_)
-
-instance Arbitrary a => Arbitrary (Row a) where
-    arbitrary = do
-        r <- arbitrary
-        pure $ Row r
-
-instance Arbitrary a => Arbitrary (Grid a) where
-    arbitrary = do
-        g <- arbitrary
-        pure $ Grid g
 
 main :: IO ()
-main = hspec $ do
-    describe "Lib.updateGrid" $ do
-        it "updates eastwards and returns the updated grid" $ do
-            forM_ 
-                [
-                    (((1, 1, 1, 1) :: (Int, Int, Int, Int)
-                    ,(1, 1, 1, 1)
-                    ,(1, 1, 1, 1)
-                    ,(1, 1, 1, 1))
-                    ,((0, 0, 2, 2)
-                    ,(0, 0, 2, 2)
-                    ,(0, 0, 2, 2)
-                    ,(0, 0, 2, 2))
-                    ),
-                    (((1, 2, 3, 4)
-                    ,(1, 1, 2, 2)
-                    ,(1, 2, 2, 1)
-                    ,(1, 1, 1, 1))
-                    ,((1, 2, 3, 4)
-                    ,(0, 0, 2, 3)
-                    ,(0, 1, 3, 1)
-                    ,(0, 0, 2, 2))
-                    ),
-                    (((1, 2, 3, 4)
-                    ,(1, 1, 2, 2)
-                    ,(1, 2, 2, 1)
-                    ,(1, 1, 1, 1))
-                    ,((1, 2, 3, 4)
-                    ,(0, 0, 2, 3)
-                    ,(0, 1, 3, 1)
-                    ,(0, 0, 2, 2))
-                    ),
-                    (((0, 0, 1, 0)
-                    ,(0, 0, 1, 0)
-                    ,(0, 0, 1, 0)
-                    ,(0, 0, 1, 0))
-                    ,((0, 0, 0, 1)
-                    ,(0, 0, 0, 1)
-                    ,(0, 0, 0, 1)
-                    ,(0, 0, 0, 1))
-                    ),
-                    (((1, 0, 0, 0)
-                    ,(0, 1, 0, 0)
-                    ,(0, 0, 1, 0)
-                    ,(0, 0, 0, 1))
-                    ,((0, 0, 0, 1)
-                    ,(0, 0, 0, 1)
-                    ,(0, 0, 0, 1)
-                    ,(0, 0, 0, 1))
-                    ),
-                    (((0, 1, 2, 0)
-                    ,(1, 2, 0, 0)
-                    ,(1, 2, 3, 0)
-                    ,(1, 2, 3, 4))
-                    ,((0, 0, 1, 2)
-                    ,(0, 0, 1, 2)
-                    ,(0, 1, 2, 3)
-                    ,(1, 2, 3, 4))
-                    )
-                ]
-                (\(i, e) -> (updateGrid East (grid i)) `shouldBe` (grid e))
-
-        it "updates westwards and returns the updated grid" $ do
-            let i = ((1, 2, 3, 4 :: Int)
-                    ,(1, 1, 2, 2)
-                    ,(1, 2, 2, 1)
-                    ,(1, 1, 1, 1))
-                e = ((1, 2, 3, 4)
-                    ,(2, 3, 0, 0)
-                    ,(1, 3, 1, 0)
-                    ,(2, 2, 0, 0))
-                    in (updateGrid West (grid i)) `shouldBe` (grid e)
-
-        it "updates southwards and returns the updated grid" $ do
-            forM_ 
-                [
-                    (((1, 2, 3, 4)
-                    ,(1, 1, 2, 2)
-                    ,(1, 2, 2, 1)
-                    ,(1, 1, 1, 1))
-                    ,((0, 2, 0, 0)
-                    ,(0, 1, 3, 4)
-                    ,(2, 2, 3, 2)
-                    ,(2, 1, 1, 2))
-                    ),
-                    (((1, 1, 1, 1)
-                    ,(1, 1, 1, 1)
-                    ,(1, 1, 1, 1)
-                    ,(1, 1, 1, 1))
-                    ,((0, 0, 0, 0)
-                    ,(0, 0, 0, 0)
-                    ,(2, 2, 2, 2)
-                    ,(2, 2, 2, 2)))
-                ]
-                (\(i, e) -> (updateGrid South (grid i)) `shouldBe` (grid e))
-
-        it "updates northwards and returns the updated grid" $ do
-            forM_ 
-                [
-                    (((1, 1, 1, 1 :: Int)
-                    ,(1, 1, 1, 1)
-                    ,(1, 1, 1, 1)
-                    ,(1, 1, 1, 1))
-                    ,((2, 2, 2, 2)
-                    ,(2, 2, 2, 2)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0))
-                    ),
-                    (((2, 2, 2, 2)
-                    ,(2, 2, 2, 2)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0))
-                    ,((3, 3, 3, 3)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0))
-                    ),
-                    (((1, 2, 3, 4)
-                    ,(1, 2, 3, 4)
-                    ,(1, 2, 3, 4)
-                    ,(1, 2, 3, 4))
-                    ,((2, 3, 4, 5)
-                    ,(2, 3, 4, 5)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0))
-                    ),
-                    (((1, 2, 3, 4)
-                    ,(2, 1, 2, 3)
-                    ,(3, 2, 1, 2)
-                    ,(4, 3, 2, 1))
-                    ,((1, 2, 3, 4)
-                    ,(2, 1, 2, 3)
-                    ,(3, 2, 1, 2)
-                    ,(4, 3, 2, 1))
-                    ),
-                    (((1, 2, 3, 1)
-                    ,(1, 1, 1, 1)
-                    ,(1, 1, 1, 1)
-                    ,(1, 2, 2, 1))
-                    ,((2, 2, 3, 2)
-                    ,(2, 2, 2, 2)
-                    ,(0, 2, 2, 0)
-                    ,(0, 0, 0, 0))
-                    ),
-                    (((1, 2, 3, 4) :: (Int, Int, Int, Int)
-                    ,(1, 1, 2, 2)
-                    ,(1, 2, 2, 1)
-                    ,(1, 1, 1, 1))
-                    ,((2, 2, 3, 4)
-                    ,(2, 1, 3, 2)
-                    ,(0, 2, 1, 2)
-                    ,(0, 1, 0, 0))
-                    ),
-                    (((0, 0, 0, 0)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0))
-                    ,((0, 0, 0, 0)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0))
-                    )
-                ]
-                (\(i, e) -> (updateGrid North (grid i)) `shouldBe` (grid e))
-
-    describe "Lib.rotateGridRight" $ do
-        it "rotates a grid to the right" $ do
+main =
+    hspec $ do
+        describe "Lib.updateGrid" $ do
+            it "updates eastwards and returns the updated grid" $
+                forM_
+                    [ ( [ [1, 1, 1, 1] :: Row Int
+                        , [1, 1, 1, 1]
+                        , [1, 1, 1, 1]
+                        , [1, 1, 1, 1]
+                        ]
+                      , [[0, 0, 2, 2], [0, 0, 2, 2], [0, 0, 2, 2], [0, 0, 2, 2]])
+                    , ( [[1, 2, 3, 4], [1, 1, 2, 2], [1, 2, 2, 1], [1, 1, 1, 1]]
+                      , [[1, 2, 3, 4], [0, 0, 2, 3], [0, 1, 3, 1], [0, 0, 2, 2]])
+                    , ( [[1, 2, 3, 4], [1, 1, 2, 2], [1, 2, 2, 1], [1, 1, 1, 1]]
+                      , [[1, 2, 3, 4], [0, 0, 2, 3], [0, 1, 3, 1], [0, 0, 2, 2]])
+                    , ( [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]]
+                      , [[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1]])
+                    , ( [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+                      , [[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1]])
+                    , ( [[0, 1, 2, 0], [1, 2, 0, 0], [1, 2, 3, 0], [1, 2, 3, 4]]
+                      , [[0, 0, 1, 2], [0, 0, 1, 2], [0, 1, 2, 3], [1, 2, 3, 4]])
+                    ]
+                    (\(i, e) -> updateGrid East i `shouldBe` e)
+            it "updates westwards and returns the updated grid" $
+                let i =
+                        [ [1, 2, 3, 4 :: Int]
+                        , [1, 1, 2, 2]
+                        , [1, 2, 2, 1]
+                        , [1, 1, 1, 1]
+                        ]
+                    e = [[1, 2, 3, 4], [2, 3, 0, 0], [1, 3, 1, 0], [2, 2, 0, 0]]
+                 in updateGrid West i `shouldBe` e
+            it "updates southwards and returns the updated grid" $
+                forM_
+                    [ ( [[1, 2, 3, 4], [1, 1, 2, 2], [1, 2, 2, 1], [1, 1, 1, 1]]
+                      , [[0, 2, 0, 0], [0, 1, 3, 4], [2, 2, 3, 2], [2, 1, 1, 2]])
+                    , ( [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+                      , [[0, 0, 0, 0], [0, 0, 0, 0], [2, 2, 2, 2], [2, 2, 2, 2]])
+                    ]
+                    (\(i, e) -> updateGrid South i `shouldBe` e)
+            it "updates northwards and returns the updated grid" $
+                forM_
+                    [ ( [ [1, 1, 1, 1 :: Int]
+                        , [1, 1, 1, 1]
+                        , [1, 1, 1, 1]
+                        , [1, 1, 1, 1]
+                        ]
+                      , [[2, 2, 2, 2], [2, 2, 2, 2], [0, 0, 0, 0], [0, 0, 0, 0]])
+                    , ( [[2, 2, 2, 2], [2, 2, 2, 2], [0, 0, 0, 0], [0, 0, 0, 0]]
+                      , [[3, 3, 3, 3], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+                    , ( [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+                      , [[2, 3, 4, 5], [2, 3, 4, 5], [0, 0, 0, 0], [0, 0, 0, 0]])
+                    , ( [[1, 2, 3, 4], [2, 1, 2, 3], [3, 2, 1, 2], [4, 3, 2, 1]]
+                      , [[1, 2, 3, 4], [2, 1, 2, 3], [3, 2, 1, 2], [4, 3, 2, 1]])
+                    , ( [[1, 2, 3, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 2, 2, 1]]
+                      , [[2, 2, 3, 2], [2, 2, 2, 2], [0, 2, 2, 0], [0, 0, 0, 0]])
+                    , ( [ [1, 2, 3, 4 :: Int]
+                        , [1, 1, 2, 2]
+                        , [1, 2, 2, 1]
+                        , [1, 1, 1, 1]
+                        ]
+                      , [[2, 2, 3, 4], [2, 1, 3, 2], [0, 2, 1, 2], [0, 1, 0, 0]])
+                    , ( [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+                      , [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+                    ]
+                    (\(i, e) -> updateGrid North i `shouldBe` e)
+        describe "Lib.rotateGridRight" $
+            it "rotates a grid to the right" $
             forM_
-                [
-                    (((1, 1, 1, 1)
-                    ,(2, 2, 2, 2)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0))
-                    ,((0, 0, 2, 1)
-                    ,(0, 0, 2, 1)
-                    ,(0, 0, 2, 1)
-                    ,(0, 0, 2, 1))
-                    ),
-                    (((1, 2, 3, 4)
-                    ,(1, 1, 2, 2)
-                    ,(1, 2, 2, 1)
-                    ,(1, 1, 1, 1))
-                    ,((1, 1, 1, 1)
-                    ,(1, 2, 1, 2)
-                    ,(1, 2, 2, 3)
-                    ,(1, 1, 2, 4)))
+                [ ( [[1, 1, 1, 1], [2, 2, 2, 2], [0, 0, 0, 0], [0, 0, 0, 0]]
+                  , [[0, 0, 2, 1], [0, 0, 2, 1], [0, 0, 2, 1], [0, 0, 2, 1]])
+                , ( [[1, 2, 3, 4], [1, 1, 2, 2], [1, 2, 2, 1], [1, 1, 1, 1]]
+                  , [[1, 1, 1, 1], [1, 2, 1, 2], [1, 2, 2, 3], [1, 1, 2, 4]])
                 ]
-                (\(i, e) -> rotateGridRight (grid i) `shouldBe`(grid e))
-
-    describe "Lib.rotateGridLeft" $ do
-        it "rotates a grid to the left" $ do
-            forM_
-                [
-                    (((1, 1, 1, 1)
-                    ,(2, 2, 2, 2)
-                    ,(0, 0, 0, 0)
-                    ,(0, 0, 0, 0))
-                    ,((1, 2, 0, 0)
-                    ,(1, 2, 0, 0)
-                    ,(1, 2, 0, 0)
-                    ,(1, 2, 0, 0))
-                    ),
-                    (((1, 2, 3, 4)
-                    ,(1, 1, 2, 2)
-                    ,(1, 2, 2, 1)
-                    ,(1, 1, 1, 1))
-                    ,((4, 2, 1, 1)
-                    ,(3, 2, 2, 1)
-                    ,(2, 1, 2, 1)
-                    ,(1, 1, 1, 1)))
-                ]
-                (\(i, e) -> rotateGridLeft (grid i) `shouldBe`(grid e))
-
-        it "is the inverse of rotateGridRight" $ property $
-             \x -> (x :: Grid Int) == rotateGridLeft (rotateGridRight x)
-
-    describe "Lib.reverse'" $ do
-        it "reverses Grids" $ do
-            reverse' (grid ((1, 1, 1, 1), (2, 2, 2, 2), (3, 3, 3, 3), (4, 4, 4, 4)))
-                `shouldBe`
-                (grid ((4, 4, 4, 4), (3, 3, 3, 3), (2, 2, 2, 2), (1, 1, 1, 1)))
-        it "is its own inverse function" $ property $ 
-             \x -> (x :: Grid Int) == reverse' (reverse' x)
-
-    describe "Lib.reverse''" $ do
-        it "reverses rows" $ do
-            reverse'' (Row (1, 2, 3, 4)) `shouldBe` (Row (4, 3, 2, 1))
-        it "is its own inverse function" $ property $ 
-             \x -> (x :: Row Int) == reverse'' (reverse'' x)
-
-    describe "Lib.reverseColumns'" $ do
-        it "reverses the columns in a Grid" $ do
-            reverseColumns (grid ((1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 16)))
-                `shouldBe` 
-                grid ((4, 3, 2, 1), (8, 7, 6, 5), (12, 11, 10, 9), (16, 15, 14, 13))
-        it "is its own inverse function" $ property $ 
-             \x -> (x :: Grid Int) == reverseColumns (reverseColumns x)
+                (\(i, e) -> rotateGridRight i `shouldBe` e)
+        describe "Lib.rotateGridLeft" $ do
+            it "rotates a grid to the left" $
+                forM_
+                    [ ( [[1, 1, 1, 1], [2, 2, 2, 2], [0, 0, 0, 0], [0, 0, 0, 0]]
+                      , [[1, 2, 0, 0], [1, 2, 0, 0], [1, 2, 0, 0], [1, 2, 0, 0]])
+                    , ( [[1, 2, 3, 4], [1, 1, 2, 2], [1, 2, 2, 1], [1, 1, 1, 1]]
+                      , [[4, 2, 1, 1], [3, 2, 2, 1], [2, 1, 2, 1], [1, 1, 1, 1]])
+                    ]
+                    (\(i, e) -> rotateGridLeft i `shouldBe` e)
+            it "is the inverse of rotateGridRight" $
+                forAll (vectorOf 4 $ vector 4) $ \x ->
+                    (x :: Grid Int) == rotateGridLeft (rotateGridRight x)
+        describe "Lib.reverseColumns'" $ do
+            it "reverses the columns in a Grid" $
+                reverseColumns
+                    [ [1, 2, 3, 4]
+                    , [5, 6, 7, 8]
+                    , [9, 10, 11, 12]
+                    , [13, 14, 15, 16]
+                    ] `shouldBe`
+                [[4, 3, 2, 1], [8, 7, 6, 5], [12, 11, 10, 9], [16, 15, 14, 13]]
+            it "is its own inverse function" $
+                property $ \x ->
+                    (x :: Grid Int) == reverseColumns (reverseColumns x)
