aa272298b36b727c2c718b160858b0ade4bf3e9e
Philippe Hürlimann <p@hurlimann.org>
2019-06-20 23:19:44 +0200

    fixed row merging and almost got board extension after move working

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE OverloadedStrings, OverloadedLabels, MultiWayIf #-}
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
 
 module Lib
     ( someFunc
+    , updateWest
     , updateGrid
     , Direction(..)
     , Grid
@@ -9,25 +10,25 @@ module Lib
     , rotateGridLeft
     , rotateGridRight
     , reverseColumns
-    ) where
+    )
+where
 
-import Control.Monad (forever)
-import Data.GI.Base
-import Data.List (genericIndex)
-import qualified Data.Text as T
-import Data.Tuple.Select
-import Data.Word (Word32)
-import qualified GI.Gdk as Gdk
-import qualified GI.Gtk as Gtk
-import qualified Reactive.Banana as B
-import qualified Reactive.Banana.Frameworks as B
-import qualified System.Random as R
+import           Control.Lens            hiding ( set )
+import           Control.Monad                  ( forever )
+import           Data.GI.Base
+import           Data.List                      ( genericIndex )
+import qualified Data.Text                     as T
+import           Data.Word                      ( Word32 )
+import qualified GI.Gdk                        as Gdk
+import qualified GI.Gtk                        as Gtk
+import qualified Reactive.Banana               as B
+import qualified Reactive.Banana.Frameworks    as B
+import qualified System.Random                 as R
 
-data Direction
-    = North
-    | East
-    | South
-    | West
+data Direction = North
+               | East
+               | South
+               | West
     deriving (Eq, Show, Read)
 
 reverseColumns :: Grid a -> Grid a
@@ -38,24 +39,24 @@ type Row a = [a]
 type Grid a = [Row a]
 
 initialGrid :: Grid Int
-initialGrid = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+initialGrid = [[0, 0, 0, 0], [2, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]]
 
-gridLookup :: Integral i => i -> i -> Grid a -> a
-gridLookup x y g = g `genericIndex` y `genericIndex` x
+gridLookup :: Integral i => Grid a -> i -> i -> a
+gridLookup g x y = g `genericIndex` y `genericIndex` x
 
 bindings =
-    [ (Gdk.KEY_Up, North)
-    , (Gdk.KEY_W, North)
-    , (Gdk.KEY_w, North)
+    [ (Gdk.KEY_Up   , North)
+    , (Gdk.KEY_W    , North)
+    , (Gdk.KEY_w    , North)
     , (Gdk.KEY_Right, East)
-    , (Gdk.KEY_D, East)
-    , (Gdk.KEY_d, East)
-    , (Gdk.KEY_Down, South)
-    , (Gdk.KEY_S, South)
-    , (Gdk.KEY_s, South)
-    , (Gdk.KEY_Left, West)
-    , (Gdk.KEY_A, West)
-    , (Gdk.KEY_a, West)
+    , (Gdk.KEY_D    , East)
+    , (Gdk.KEY_d    , East)
+    , (Gdk.KEY_Down , South)
+    , (Gdk.KEY_S    , South)
+    , (Gdk.KEY_s    , South)
+    , (Gdk.KEY_Left , West)
+    , (Gdk.KEY_A    , West)
+    , (Gdk.KEY_a    , West)
     ]
 
 keyCodeToDirection :: Word32 -> Maybe Direction
@@ -64,30 +65,26 @@ keyCodeToDirection keycode = lookup keycode bindings
 someFunc :: IO ()
 someFunc = do
     Gtk.init Nothing
-    window <-
-        new
-            Gtk.ApplicationWindow
-            [ #title := "Reactive banana and gtk test"
-            , #windowPosition := Gtk.WindowPositionCenter
-            ]
+    window <- new Gtk.ApplicationWindow
+        [ #title := "Reactive banana and gtk test"
+        , #windowPosition := Gtk.WindowPositionCenter
+        ]
     on window #destroy Gtk.mainQuit
-    labelGrid <-
-        new
-            Gtk.Grid
-            [ #expand := True
-            , #halign := Gtk.AlignFill
-            , #valign := Gtk.AlignFill
-            ]
+    labelGrid <- new Gtk.Grid
+        [ #expand := True
+        , #halign := Gtk.AlignFill
+        , #valign := Gtk.AlignFill
+        ]
     #add window labelGrid
-    labels <-
-        sequence
-            [ let coord = (x, y)
-                  t = T.pack $ show coord
-               in (,) coord <$> new Gtk.Label [#label := t, #expand := True]
-            | x <- [0 .. 3]
-            , y <- [0 .. 3]
-            ]
+    labels <- sequence
+        [ let coord = (x, y)
+              t     = T.pack $ show coord
+          in  (,) coord <$> new Gtk.Label [#label := t, #expand := True]
+        | x <- [0 .. 3]
+        , y <- [0 .. 3]
+        ]
     mapM_ (\((x, y), label) -> #attach labelGrid label x y 1 1) labels
+    displayGrid initialGrid labels
     (keyEvent, fireKey) <- B.newAddHandler
     on window #keyPressEvent $ \eventKey -> do
         kv <- get eventKey #keyval
@@ -97,64 +94,62 @@ someFunc = do
     let networkDescription :: B.MomentIO ()
         networkDescription = do
             eKey <- B.fromAddHandler keyEvent
-            let eDir = B.filterJust $ fmap keyCodeToDirection eKey
+            let eDir        = B.filterJust $ fmap keyCodeToDirection eKey
             let eGridChange = fmap updateGrid eDir
-            eGrid <- B.accumE initialGrid eGridChange
+            let eGridChangeAndUpdate =
+                    ((extendGrid $ R.mkStdGen 0) .) <$> eGridChange
+            eGrid <- B.accumE initialGrid eGridChangeAndUpdate
             B.reactimate $ (`displayGrid` labels) <$> eGrid
     network <- B.compile networkDescription
     B.actuate network
     Gtk.main
 
-displayGrid ::
-       (Integral i, Num a, Show a) => Grid a -> [((i, i), Gtk.Label)] -> IO ()
-displayGrid grid =
-    mapM_
-        (\((x, y), label) ->
-             let val = T.pack $ show $ gridLookup x y grid
-              in set label [#label := val])
+extendGrid :: (Num a, Eq a) => R.StdGen -> Grid a -> Grid a
+extendGrid r grid = modifyGrid grid x y n
+  where
+    (foo, r') = R.randomR (0, 9 :: Int) r
+    n         = if foo == 0 then 2 else 1
+    choices =
+        [ (x, y) | x <- [0 .. 3], y <- [0 .. 3], gridLookup grid x y == 0 ]
+    (i, _) = R.randomR (0, length choices - 1) r'
+    (x, y) = choices !! i
+
+displayGrid
+    :: (Integral i, Integral a, Show a)
+    => Grid a
+    -> [((i, i), Gtk.Label)]
+    -> IO ()
+displayGrid grid = mapM_
+    (\((x, y), label) ->
+        let val    = 2 ^ gridLookup grid x y
+            label' = if val == 1 then "" else T.pack $ show val
+        in  set label [#label := label']
+    )
 
 updateGrid :: (Eq a, Num a) => Direction -> Grid a -> Grid a
-updateGrid dir grid =
-    case dir of
-        East -> map updateEast grid
-        South -> rotateGridRight $ updateGrid East $ rotateGridLeft grid
-        West -> reverseColumns $ updateGrid East $ reverseColumns grid
-        North -> reverse $ updateGrid South $ reverse grid
-  where
-    updateEast :: (Eq a, Num a) => Row a -> Row a
-    updateEast (a:b:c:d:_) =
-        if | a /= 0 && c /= 0 && a == b && c == d -> [0, 0, a + 1, c + 1]
-           | c /= 0 && c == d -> [0, a, b, c + 1]
-           | b /= 0 && b == c -> [0, a, b + 1, d]
-           | a /= 0 && a == b -> [0, a + 1, c, d]
-           | a == 0 && b == 0 && d == 0 -> [0, 0, 0, c]
-           | a == 0 && c == 0 && d == 0 -> [0, 0, 0, b]
-           | b == 0 && c == 0 && d == 0 -> [0, 0, 0, a]
-           | a == 0 && b == 0 -> [0, 0, c, d]
-           | a == 0 && c == 0 -> [0, 0, b, d]
-           | a == 0 && d == 0 -> [0, 0, b, c]
-           | b == 0 && c == 0 -> [0, 0, a, d]
-           | b == 0 && d == 0 -> [0, 0, a, c]
-           | c == 0 && d == 0 -> [0, 0, a, b]
-           | b == 0 -> [0, a, c, d]
-           | c == 0 -> [0, a, b, d]
-           | d == 0 -> [0, a, b, c]
-           | otherwise -> [a, b, c, d]
-    --n <- (\r -> if r == 0 then 2 else 1) <$> R.randomRIO(0, 9)
-    --let choices = [(x, y) | x <- [0..3], y <- [0..3], gridLookup x y == 0]
-    --i <- R.randomRIO (0, length choices - 1)
-    --let (x, y) = choices !! i
-    --let newGrid = (modifyGrid x y) n grid
-    --pure newGrid
+updateGrid dir grid = case dir of
+    West  -> map updateWest grid
+    South -> rotateGridRight $ updateGrid East $ rotateGridLeft grid
+    East  -> reverseColumns $ updateGrid West $ reverseColumns grid
+    North -> reverse $ updateGrid South $ reverse grid
 
---extendGrid :: Num a => Grid a -> IO (Grid a)
---extendGrid grid = do
+updateWest :: (Eq a, Num a) => Row a -> Row a
+updateWest xs = take 4 $ ue (filter (/= 0) xs) ++ repeat 0
+  where
+    ue []  = []
+    ue [a] = [a]
+    ue (a : b : xs) | a == b    = (a + 1) : ue xs
+                    | otherwise = a : ue (b : xs)
+
+modifyGrid :: Grid a -> Int -> Int -> a -> Grid a
+modifyGrid grid x y n = grid & (element y . element x) .~ n
+
 rotateGridRight :: Grid a -> Grid a
 rotateGridRight g =
     let ((a1:a2:a3:a4:_):(b1:b2:b3:b4:_):(c1:c2:c3:c4:_):(d1:d2:d3:d4:_):_) = g
-     in [[d1, c1, b1, a1], [d2, c2, b2, a2], [d3, c3, b3, a3], [d4, c4, b4, a4]]
+    in  [[d1, c1, b1, a1], [d2, c2, b2, a2], [d3, c3, b3, a3], [d4, c4, b4, a4]]
 
 rotateGridLeft :: Grid a -> Grid a
 rotateGridLeft g =
     let ((a1:a2:a3:a4:_):(b1:b2:b3:b4:_):(c1:c2:c3:c4:_):(d1:d2:d3:d4:_):_) = g
-     in [[a4, b4, c4, d4], [a3, b3, c3, d3], [a2, b2, c2, d2], [a1, b1, c1, d1]]
+    in  [[a4, b4, c4, d4], [a3, b3, c3, d3], [a2, b2, c2, d2], [a1, b1, c1, d1]]
